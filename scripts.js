3a35d9792b5f23a066e975c77c18e8fca19a8135
York <sc17yxm@leeds.ac.uk>
2024-07-16 12:34:08 +0100

    create files and placeholder functions

---
+++ scripts.js
@@ -0,0 +1,40 @@
+let firstNum = 0;
+let operator = '';
+let secondNum = 0;
+
+function add(a,b) {
+    return a + b;
+}
+
+function subtract(a,b) {
+    return a - b;
+}
+
+function multiply(a,b) {
+    return a * b;
+}
+
+function divide(a,b) {
+    if (b === 0) return 'ERROR';
+    return a / b;
+}
+
+function operate(first, second, operator) {
+    switch (operator) {
+        case '+':
+            
+            break;
+    
+        case '-':
+            break;
+
+        case '*':
+            break;
+        
+        case '/':
+            break;
+
+        default:
+            break;
+    }
+}
